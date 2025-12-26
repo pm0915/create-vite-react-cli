d6005df17ea2a96863cd8754f680a05995ec3b64
panmins (Personal) <minpan1206@gmail.com>
2025-12-26 17:59:31 +0800

    chore: 将版本号调整为 0.1.0
    - 按照语义化版本规范，将版本从 1.0.2 调整为 0.1.0
    - 表示项目处于早期开发阶段

--- bin/cli.js
+++ bin/cli.js
@@ -6,7 +6,7 @@ const createProject = require('../src/index');
 program
   .name('create-vite-react-cli')
   .description('快速创建基于 Vite 的 React 项目')
-  .version('1.0.2')
+  .version('0.1.0')
   .argument('[project-name]', '项目名称')
   .action((projectName) => {
     createProject(projectName);
