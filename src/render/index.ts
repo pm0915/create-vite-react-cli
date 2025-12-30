9731eacad716edb9bb25272976b94d64dbbc3931
panmin <panmin05@chinasoftinc.com>
2025-12-30 20:30:23 +0800

    chore: add tslib dependency and remove debug logs from index.ts

--- src/render/index.ts
+++ src/render/index.ts
@@ -20,10 +20,7 @@ function processProjectFiles(result) {
 
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = path.dirname(__filename)
-  console.log('__filename:', __filename)
-  console.log('__dirname:', __dirname)
   const templateRoot = path.join(__dirname, './templates')
-  console.log('templateRoot:', templateRoot)
 
   const ejsFiles: {
     src: string
